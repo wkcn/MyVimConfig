80a4f6aeb11c3024e4c250cb5be31e819ba81345
Kan Wu (MSR Student-FA Talent) <FAREAST.v-wukan@GCRAZGDL174.redmond.corp.microsoft.com>
2017-07-30 07:00:56 +0000

    add install file

---
+++ install.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/.vim/_undodir
+mkdir -p ~/.vim/colors
+mkdir -p ~/.vim/bundle
+cp mirai.vim  ~/.vim/colors/
+cp .vimrc ~/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
