864ad62e33e7a2aef35e533986c7bdcdb6239f25
Kan Wu (MSR Student-FA Talent) <FAREAST.v-wukan@GCRAZGDL174.redmond.corp.microsoft.com>
2017-10-31 01:38:31 +0000

    install luna theme

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ mkdir -p ~/.vim/_undodir
 mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/bundle
 cp mirai.vim  ~/.vim/colors/
+cp -r autoload/ ~/.vim
 cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
