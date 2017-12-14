3172b840d59bc6de6ae53e3b4ea87fef753534cc
wkcn <wkcn@live.cn>
2017-12-14 09:17:32 +0800

    fix vundle name

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.vim/bundle
 cp mirai.vim  ~/.vim/colors/
 cp -r autoload/ ~/.vim
 cp .vimrc ~/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
